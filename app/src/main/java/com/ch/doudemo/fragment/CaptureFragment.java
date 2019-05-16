fc0a4e7456147308e646d60a7fca724275847e1b
zhanghehe <709947764@qq.com>
2019-05-16 22:13:51 +0800

    a

---
+++ app/src/main/java/com/ch/doudemo/fragment/CaptureFragment.java
@@ -0,0 +1,22 @@
+package com.ch.doudemo.fragment;
+
+/**
+ * Create by alex on 2019/5/16
+ * desc:
+ */
+public class CaptureFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
